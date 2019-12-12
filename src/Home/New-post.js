cc91be6a626914a3bdef73693a26330d2e1b5cf0
amincoba <pdigitaic@gmail.com>
2019-12-12 13:35:33 +0700

    menambahakan gambar di new post

--- src/Home/New-post.js
+++ src/Home/New-post.js
@@ -30,16 +30,21 @@ export default function Newpost() {
 
   const articles = [
     {
+      image:
+        "https://yt3.ggpht.com/a/AGF-l7-9-FRpBK_E7OQcduUOufKvX3SEXv8svC1Cfg=s900-c-k-c0xffffffff-no-rj-mo",
       category: "web",
       title: "Blogger",
       content: "blogger is the one of most polular blog maker.."
     },
     {
+      image:
+        "https://ultrawebjogja.com/wp-content/uploads/2019/07/wordpress.png",
       category: "web",
       title: "WordPress",
       content: "WordPress is the one of most polular blog maker.."
     },
     {
+      image: "https://medium.com/icons/monogram-mask.svg",
       category: "web",
       title: "Medium",
       content: "Medium is the one of most polular blog maker.."
@@ -53,7 +58,7 @@ export default function Newpost() {
         <div className="new-article">
           <CardContent>
             <div className="image-new-post">
-              <img src="{articles.image}"></img>
+              <img src={article.image}></img>
             </div>
             <Typography
               className={classes.title}
