6f11d424570a1665671a2ca64acc561e8afc5ada
amincoba <pdigitaic@gmail.com>
2019-12-23 11:41:38 +0700

    edit Article.js

--- src/Article.js
+++ src/Article.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 import ArticleBar from "./Home/Article-bar";
 import Claps from "./Article-part/Claps";
 import "./App.css";
-import "./Home/Home.css";
+// import "./Home/Home.css";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -33,13 +33,6 @@ function Article() {
     {
       image: "https://cdn-image.bisnis.com/posts/2018/05/21/797564/arab.jpg",
       nama: "Salman",
-      comment:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    },
-    {
-      image:
-        "https://yt3.ggpht.com/a/AGF-l7-Xf5FxKw2vayMhrqnz0gr5DAcnUjrK9ZDpFQ=s900-c-k-c0xffffffff-no-rj-mo",
-      nama: "Ria Ricis",
       comment:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
