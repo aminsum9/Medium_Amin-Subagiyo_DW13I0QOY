b6fe1a0f724dfef6e5787fe627be39122ae4abc5
Amin Subagiyo <aminsum9@gmail.com>
2023-01-27 20:32:08 +0700

    update app

--- src/Home/Post.js
+++ src/Home/Post.js
@@ -30,7 +30,7 @@ export default function Post() {
   return (
     <Grid container className={classes.root} spacing={2}>
       <Grid item xs={12}>
-        <Grid container justify="center" spacing={spacing}>
+        <Grid container justifyContent="center" spacing={spacing}>
           <Grid item xs={8}>
             <Paper className={classes.paper}>
               <LastPost />
--- src/Menu-profile/New-story.js
+++ src/Menu-profile/New-story.js
@@ -26,8 +26,6 @@ const Newstory = (props) => {
 
   const hiddenFileInput = React.useRef(null);
 
-  // Programatically click the hidden file input element
-  // when the Button component is clicked
   const handleClick = (event) => {
     hiddenFileInput.current.click();
   };
